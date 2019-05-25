8724ed61e7141d1d22dfe4a2e42138e5eed7611a
asangai <asangai@kyrance.com>
2019-05-26 00:52:12 +0530

    view ok

--- viewitem.js
+++ viewitem.js
@@ -1,7 +1,6 @@
 $(function () {
 
 	getItems();
-	var xhttp = new XMLHttpRequest();
 
 });
 
@@ -9,15 +8,11 @@ function getItems() {
 	$.ajax({
 		type: 'GET',
 		url: 'http://localhost:8080/shopping/items',
-		headers: {
-			"accept": "application/json",
-			"content-type": "application/x-www-form-urlencoded"
-		},
 		dataType: 'json',
 		async: true,
 		success: function (data) {
 			console.log(data);
-
+			setItems(data)
 		},
 		error:function (error) {
 			console.log('error ${error}')
@@ -26,35 +21,29 @@ function getItems() {
 
 	});
 }
-function setStudents(students) {
+function setItems(items) {
 	// if ($.fn.DataTable.isDataTable('#tblStudent')) {
 	//     $('#tblStudent').DataTable().destroy();
 	// }
-	$('#tblStudent tbody').empty();
+	$('#tblItems tbody').empty();
 	var textToInsert = '';
-	for (var item in students) {
-		textToInsert += addRowToSearchStudent(students[item]);
+	for (var item in items) {
+		textToInsert += addRowToItems(items[item]);
 	}
-	$('#tblStudent > tbody:last-child').append(textToInsert);
-	$('#tblStudent').dataTable({
-		order: [[0, "asc"]],
-		select: true
+	$('#tblItems > tbody:last-child').append(textToInsert);
 
-	});
 }
-function addRowToSearchStudent(item) {
-	var baseurl = document.getElementById('baseurl').value;
+function addRowToItems(item) {
 //    console.log(item.ItemCode);
 	var row =
-		'<tr id="' + item.id + '" class="openPane">'
-		+ '<td>' + item.id + '</td>'
+		'<tr id="' + item.item_id + '" class="openPane">'
+		+ '<td>' + item.item_id + '</td>'
 		+ '<td>' + item.name + '</td>'
-		+ '<td>' + item.faculty + '</td>'
-		+ '<td>' + item.tel + '</td>'
-		+ '<td>' + item.nic + '</td>'
-		+ '<td>'
-		+ '<a href="' + baseurl + 'student/newstudent?id=' + item.id + '"><button type="button" class="btn btn-info btn-xs">Edit</button></a>'
-		+ '</td>'
+		+ '<td>' + item.description + '</td>'
+		+ '<td>' + item.sup_id + '</td>'
+		+ '<td>' + item.qty + '</td>'
+		+ '<td>' + item.unit_price + '</td>'
+		+ '<td>' + item.visibility + '</td>'
 		+ '</tr>';
 	return row;
 }
